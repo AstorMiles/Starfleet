f0fe3d49e987a2e9d1553e01ea661eb7dc8416e6
AstorMiles <70938417+AstorMiles@users.noreply.github.com>
2022-07-11 17:25:08 +0300

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,9 @@
 <template>
      <div class="bg">
      </div> 
-  <!-- <div class="home"></div> -->
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
 
 <style>
 
